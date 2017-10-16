c80fe313dcfb5755d3e9c6cc9a792b2f46f004e3
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-16 19:48:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function whisper(string) {
 }
 
 function logWhisper(string) {
-  console.log('I can\'t hear you!')
+  console.log('hello')
+}
+
+function logShout(string) {
+  console.log('HELLO')
 }
 
 function sayHiToGrandma(string) {
