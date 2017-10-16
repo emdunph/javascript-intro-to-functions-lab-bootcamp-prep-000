fc896dd8a7bc7ee6917a33256fb83689be82b9fd
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-16 19:44:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string) {
-  console.log('HELLO!')
+
+function logWhisper(string) {
+  console.log('I can\'t hear you!')
+}
+
+function sayHiToGrandma(string) {
+  if string === string.toUppercase() return('YES INDEED!')
 }
